f8b8c55cf24bdee34c43f6791c8826fd0b26f2f0
antlikelion <ignorant_woo@naver.com>
2019-10-05 02:23:16 +0900

    컴포넌트에 생략할 수 있는 props설정하

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 type GreetingsProps = {
     name: string
     mark: string
+    optional?: string
 }
 
 // React.FC를 안 쓰고도 충분히 컴포넌트를 만들 수 있다!
-function Greetings({name, mark}:GreetingsProps){
+function Greetings({name, mark, optional }:GreetingsProps){
     return(
         <div>
             Hello, {name} {mark}
+            {optional && <p>{optional}</p>}
         </div>
     )
 }
