61227fa8af0cb5db542a1778fe24167eec0d1425
antlikelion <ignorant_woo@naver.com>
2019-10-05 02:32:44 +0900

    컴포넌트에서 함수 타입의 props받아오기

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -4,14 +4,20 @@ type GreetingsProps = {
     name: string
     mark: string
     optional?: string
+    onClick: (name:string)=>void
+    // 아무것도 반환하지 않는 함수
 }
 
 // React.FC를 안 쓰고도 충분히 컴포넌트를 만들 수 있다!
-function Greetings({name, mark, optional }:GreetingsProps){
+function Greetings({name, mark, optional, onClick }:GreetingsProps){
+    const handleClick = () => onClick(name)
     return(
         <div>
             Hello, {name} {mark}
             {optional && <p>{optional}</p>}
+            <div>
+                <button onClick={handleClick}>Click Me</button>
+            </div>
         </div>
     )
 }
