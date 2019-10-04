fa4fbb61911cabff1e94dc32770deaeab629eb33
antlikelion <ignorant_woo@naver.com>
2019-10-05 03:41:26 +0900

    useReducer심화

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 
 const App: React.FC = () => {
-  const onSubmit = (form: {name:string; description:string})=>{
-    console.log(form);
-  }
   return (
-    <MyForm onSubmit={onSubmit} />
+    <ReducerSample />
   );
 }
 
