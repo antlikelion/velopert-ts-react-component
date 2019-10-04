cc6d846807373f7070981fe7671910e508e28986
antlikelion <ignorant_woo@naver.com>
2019-10-05 03:18:57 +0900

    ts-react에서의 useReducer

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,17 +1,24 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+type Action = {type:'INCREASE'} | {type:"DECREASE"}
+// 액션은 |(or) 로 나열하면 된다
+
+// 아래처럼 reducer를 만들 때에는 state의 타입과 reducer의 반환하는 타입이 일치해야 한다
+function reducer(state:number, action: Action): number {
+    switch(action.type){
+        case 'INCREASE':
+            return state + 1;
+        case 'DECREASE':
+            return state - 1;
+        default:
+            throw new Error('Unhandled action')
+    }
+}
 
 function Counter() {
-    const [count, setCount] = useState<number>(0);
-    // generic이 적용되는 것을 제외하면 js와 별 차이가 없다
-    // 심지어 generic을 생략해도 알아서 타입 유추를 해준다
-    // 따라서 상태가 null 일 수도, 아닐수도 있을 때 generic을 활용하면 좋다(밑처럼)
-    // const [count, setCount] = useState<number|null>(0);
-    // 또한 상태의 타입이 까다로운 구조를 가진 객체거나 배열일 경우엔 generic을 명시해주는 게 좋다(밑처럼)
-    // type Todos = {id:number; text:string; done:boolean}
-    // const [todos, setTodos] = useState<Todos[]>([])
-
-    const onIncrease = () => setCount(count + 1)
-    const onDecrease = () => setCount(count - 1)
+    const [count, dispatch] = useReducer(reducer, 0)
+    const onIncrease = () => dispatch({type:'INCREASE'})
+    const onDecrease = () => dispatch({type:'DECREASE'})
     return (
         <div>
             <h1>{count}</h1>
